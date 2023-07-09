3e8d2810e8c3dd859702d90917ad4ee02ca5e70c
fernaldr <121467203+fernaldr@users.noreply.github.com>
2023-07-09 17:48:27 -0400

    updated index

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,5 @@
 import { Html, Head, NextScript } from 'next/document'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+
 
 export default function Document() {
   return (
@@ -13,12 +8,6 @@ export default function Document() {
       <Head />
       <body className='text-black'>
         <NextScript />
-        <Header />
-        <Hero />
-        <About />
-        <Projects />
-        <Contact />
-        <Footer />
       </body>
     </Html>
   )
